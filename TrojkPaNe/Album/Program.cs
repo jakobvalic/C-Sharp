1b16dc8fae9348c1a1b7e2048db1fe4671bb9222
Jakob Valič <jakob.valic@gmail.com>
2017-11-09 22:22:23 +0100

    Dodal 4. vajo do konca

--- TrojkPaNe/Album/Program.cs
+++ TrojkPaNe/Album/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 
 namespace Album
 {
@@ -12,29 +11,58 @@ namespace Album
 
         static void Main(string[] args)
         {
-            int[] t = Album(10, 40);
-            Console.WriteLine(String.Join(" ", t));
+            for (int i=0; i<1000; i++)
+            {
+                int velikostAlbuma = 200;
+                Console.Write(String.Format("Polnjenje albuma številka {0}: za {1} sličic smo morali kupiti: ", i + 1, velikostAlbuma));
+                System.Threading.Thread.Sleep(100);
+                List<int> t = AlbumMnozica(velikostAlbuma, 10); // Drugače vrne isti rezultat za več zaporednih klicev funkcije.
+                //Console.WriteLine(String.Join(" ", t));
+            }
         }
 
-
-        static int[] Album(int velikostAlbuma, int velikostPaketa)
+        /// <summary>
+        /// Že izžrebane številke shranjuje v množico. Vrne število sličic, ki smo jih kupili.
+        /// </summary>
+        /// <param name="velikostAlbuma"></param>
+        /// <param name="velikostPaketa"></param>
+        /// <returns></returns>
+        static List<int> AlbumMnozica(int velikostAlbuma, int velikostPaketa)
         {
+            int stevecKartic = 0;
             Random genNaklj = new Random();
-            int[] kupimo = new int[velikostPaketa];
-            for (int i=0; i<velikostPaketa; i++)
+            HashSet<int> razlicne = new HashSet<int>();
+            while (razlicne.Count != velikostAlbuma)
             {
-                kupimo[i] = i;
+                int novoNaklj = genNaklj.Next(0, velikostAlbuma) + 1;
+                // Console.Write(novoNaklj);
+                razlicne.Add(novoNaklj);
+                stevecKartic++;
             }
-            for (int i=0; i<velikostAlbuma; i++)
-            {
-                int izbira = genNaklj.Next(i, velikostPaketa);
-                int pomozni = kupimo[i];
-                kupimo[i] = kupimo[izbira];
-                kupimo[izbira] = pomozni;
-            }
-            return kupimo.Take(velikostAlbuma).ToArray();
-
-              
+            List<int> urejenSeznam = razlicne.ToList();
+            urejenSeznam.Sort();
+            Console.WriteLine(stevecKartic);
+            return urejenSeznam;
         }
+
+
+
+        //static int[] AlbumAlgoritem(int velikostAlbuma, int velikostPaketa)
+        //{
+        //    Random genNaklj = new Random();
+        //    int[] kupimo = new int[velikostPaketa];
+        //    for (int i=0; i<velikostPaketa; i++)
+        //    {
+        //        kupimo[i] = i;
+        //    }
+        //    for (int i=0; i<velikostAlbuma; i++)
+        //    {
+        //        int izbira = genNaklj.Next(i, velikostPaketa);
+        //        int pomozni = kupimo[i];
+        //        kupimo[i] = kupimo[izbira];
+        //        kupimo[izbira] = pomozni;
+        //    }
+        //    return kupimo.Take(velikostAlbuma).ToArray();    
+        //}
     }
 }
