3d39023091447692471b7725e51ce8de835dc4d8
Jakob Valič <jakob.valic@gmail.com>
2017-11-10 10:02:07 +0100

    zacetek 5. vaje

---
+++ 5-vaja/5-vaja/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_vaja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LovljenjeNapak();
+            
+        }
+
+        static void LovljenjeNapak()
+        {
+            Console.WriteLine("Program izpiše vrednost izraza (a+b)/c, za cela števila a,b,c");
+            try
+            {
+                Console.Write("Vnesi a: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Vnesi b: ");
+                int b = int.Parse(Console.ReadLine());
+                Console.Write("Vnesi c: ");
+                int c = int.Parse(Console.ReadLine());
+                Console.WriteLine("(a+b)/c = " + ((a + b) / c));
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine("Vnesti morate število in ne drugih znakov.");
+                Console.WriteLine(e.ToString()); // Koristno, da vemo, v kateri vrstici se je napaka zgodila.
+                LovljenjeNapak();
+            }
+            catch (System.DivideByZeroException e)
+            {
+                Console.WriteLine("Deljenje z ničlo ni dovoljeno.");
+                Console.WriteLine(e.ToString());
+                LovljenjeNapak();
+            }
+            catch (System.OverflowException e)
+            {
+                Console.WriteLine("Presegli ste omejitev za tip int.");
+                Console.WriteLine(e.ToString());
+                LovljenjeNapak();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Nekaj drugega je šlo narobe:");
+                Console.WriteLine(e.ToString());
+                // Tu zaustavimo delovanje programa, ker program še ni dopolnjen.
+            }
+        }
+    }
+}
---
+++ 5-vaja/Datoteke/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Datoteke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pot = @"\\Spin\ValicJ15$\_System\Desktop\___\vnos1";
+            ZapisiIzpisi(pot);
+        }
+
+        static void ZapisiIzpisi(String pot)
+        {
+            StreamWriter dat;
+            dat = File.AppendText(pot); // Če datoteke ni, jo naredi :)
+            String vnos = "";
+            while (vnos.ToLower() != "konec")
+            {
+                Console.Write("Napiši nekaj: ");
+                if (vnos.ToLower() != "konec")
+                {
+                    dat.WriteLine(vnos);
+                }
+                vnos = Console.ReadLine();
+            }
+            dat.Close();
+            using (StreamReader sr = File.OpenText(pot))
+            {
+                int stevec = 1;
+                String vrstica = sr.ReadLine();
+                while (vrstica != null)
+                {
+                    Console.WriteLine(stevec + ".vrstica: " + vrstica);
+                    vrstica = sr.ReadLine();
+                    stevec++;
+                }
+            }
+            using (StreamReader sr = File.OpenText(pot))
+            {
+                while(!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+    }
+}
