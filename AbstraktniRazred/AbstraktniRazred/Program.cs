b809a305268b492a40483edc4f36cc6014868e88
Jakob Valič <jakob.valic@gmail.com>
2017-11-14 00:26:51 +0100

    Dodal problem n kraljic. Dodal abstraktni razred.

---
+++ AbstraktniRazred/AbstraktniRazred/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstraktniRazred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trikotnik trikotnik = new Trikotnik();
+            trikotnik.opisi();
+            Lik lik = new Lik();
+        }
+    }
+
+    abstract class Lik
+    {
+        public virtual void opisi()
+        {
+            Console.Write("Katerikoli lik. ");
+        }
+    }
+
+    class Trikotnik : Lik
+    {
+
+        public override void opisi()
+        {
+            base.opisi();
+            Console.WriteLine("Bolj natančno trikotnik.");
+        }
+    }
+}
