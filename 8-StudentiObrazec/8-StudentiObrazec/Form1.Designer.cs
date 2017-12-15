00658c3729c41d4601d080d55804f94c96eebb0f
Jakob Valič <jakob.valic@gmail.com>
2017-12-15 10:04:59 +0100

    dodal vajo 8

---
+++ 8-StudentiObrazec/8-StudentiObrazec/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace _8_StudentiObrazec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_ime = new System.Windows.Forms.TextBox();
+            this.textBox_priimek = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.DataSource = this.listBox1.CustomTabOffsets;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 147);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Spol";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ženska",
+            "Moški"});
+            this.comboBox1.Location = new System.Drawing.Point(297, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Priimek";
+            // 
+            // textBox_ime
+            // 
+            this.textBox_ime.Location = new System.Drawing.Point(297, 53);
+            this.textBox_ime.Name = "textBox_ime";
+            this.textBox_ime.Size = new System.Drawing.Size(121, 20);
+            this.textBox_ime.TabIndex = 5;
+            // 
+            // textBox_priimek
+            // 
+            this.textBox_priimek.Location = new System.Drawing.Point(297, 81);
+            this.textBox_priimek.Name = "textBox_priimek";
+            this.textBox_priimek.Size = new System.Drawing.Size(121, 20);
+            this.textBox_priimek.TabIndex = 6;
+            this.textBox_priimek.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 22);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Dodaj študenta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 413);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_priimek);
+            this.Controls.Add(this.textBox_ime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_ime;
+        private System.Windows.Forms.TextBox textBox_priimek;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 8-StudentiObrazec/8-StudentiObrazec/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_StudentiObrazec
+{
+    public partial class Form1 : Form
+    {
+        BindingList<Student> b = new BindingList<Student>();
+
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Student Tomi = new Student("Tomi", "Troha", "moški", new DateTime(1990, 3, 12));
+            b.Add(Tomi);
+            // listBox1.Items.Add(Tomi);
+
+            listBox1.DataSource = b;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b.Add(new Student(textBox_ime.Text, textBox_priimek.Text, "moški", new DateTime(1990, 2, 12)));
+        }
+    }
+}
