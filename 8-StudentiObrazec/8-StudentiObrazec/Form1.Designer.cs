2c8da6c803a45edf3af6e9a720e82de654497df9
Jakob Valič <jakob.valic@gmail.com>
2017-12-19 11:48:27 +0100

    nekaj dodatkov pri študentih

--- 8-StudentiObrazec/8-StudentiObrazec/Form1.Designer.cs
+++ 8-StudentiObrazec/8-StudentiObrazec/Form1.Designer.cs
@@ -28,49 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.izbirnikSpola = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox_ime = new System.Windows.Forms.TextBox();
             this.textBox_priimek = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.izbirnikDatuma = new System.Windows.Forms.DateTimePicker();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.DataSource = this.listBox1.CustomTabOffsets;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(22, 60);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(178, 147);
-            this.listBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(247, 127);
+            this.label1.Location = new System.Drawing.Point(89, 85);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Spol";
             // 
-            // comboBox1
+            // izbirnikSpola
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.izbirnikSpola.FormattingEnabled = true;
+            this.izbirnikSpola.Items.AddRange(new object[] {
             "Ženska",
             "Moški"});
-            this.comboBox1.Location = new System.Drawing.Point(297, 119);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.izbirnikSpola.Location = new System.Drawing.Point(130, 82);
+            this.izbirnikSpola.Name = "izbirnikSpola";
+            this.izbirnikSpola.Size = new System.Drawing.Size(121, 21);
+            this.izbirnikSpola.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(247, 60);
+            this.label2.Location = new System.Drawing.Point(89, 26);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(24, 13);
             this.label2.TabIndex = 3;
@@ -79,7 +78,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(247, 88);
+            this.label3.Location = new System.Drawing.Point(89, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 13);
             this.label3.TabIndex = 4;
@@ -87,14 +86,15 @@
             // 
             // textBox_ime
             // 
-            this.textBox_ime.Location = new System.Drawing.Point(297, 53);
+            this.textBox_ime.Location = new System.Drawing.Point(130, 23);
             this.textBox_ime.Name = "textBox_ime";
             this.textBox_ime.Size = new System.Drawing.Size(121, 20);
             this.textBox_ime.TabIndex = 5;
+            this.textBox_ime.TextChanged += new System.EventHandler(this.textBox_ime_TextChanged);
             // 
             // textBox_priimek
             // 
-            this.textBox_priimek.Location = new System.Drawing.Point(297, 81);
+            this.textBox_priimek.Location = new System.Drawing.Point(130, 52);
             this.textBox_priimek.Name = "textBox_priimek";
             this.textBox_priimek.Size = new System.Drawing.Size(121, 20);
             this.textBox_priimek.TabIndex = 6;
@@ -102,7 +102,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(297, 268);
+            this.button1.Location = new System.Drawing.Point(124, 135);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 22);
             this.button1.TabIndex = 7;
@@ -110,36 +110,106 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // izbirnikDatuma
+            // 
+            this.izbirnikDatuma.Location = new System.Drawing.Point(71, 109);
+            this.izbirnikDatuma.Name = "izbirnikDatuma";
+            this.izbirnikDatuma.Size = new System.Drawing.Size(180, 20);
+            this.izbirnikDatuma.TabIndex = 8;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(600, 437);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.izbirnikDatuma);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox_ime);
+            this.tabPage1.Controls.Add(this.izbirnikSpola);
+            this.tabPage1.Controls.Add(this.textBox_priimek);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(592, 411);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "dodajanje";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Turquoise;
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(592, 411);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "prikaz";
+            // 
+            // listBox1
+            // 
+            this.listBox1.DataSource = this.listBox1.CustomTabOffsets;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(325, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 147);
+            this.listBox1.TabIndex = 10;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Nina Drmota",
+            "Martin Česnovar"});
+            this.listBox2.Location = new System.Drawing.Point(118, 70);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(270, 199);
+            this.listBox2.TabIndex = 10;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(474, 413);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox_priimek);
-            this.Controls.Add(this.textBox_ime);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(600, 437);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox izbirnikSpola;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox_ime;
         private System.Windows.Forms.TextBox textBox_priimek;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker izbirnikDatuma;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- 8-StudentiObrazec/8-StudentiObrazec/Form1.cs
+++ 8-StudentiObrazec/8-StudentiObrazec/Form1.cs
@@ -23,8 +23,7 @@ namespace _8_StudentiObrazec
             Student Tomi = new Student("Tomi", "Troha", "moški", new DateTime(1990, 3, 12));
             b.Add(Tomi);
             // listBox1.Items.Add(Tomi);
-
-            listBox1.DataSource = b;
+            listBox1.DataSource = b; // Povežemo s strukturo, iz katere črpamo podatke
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -34,7 +33,23 @@ namespace _8_StudentiObrazec
 
         private void button1_Click(object sender, EventArgs e)
         {
-            b.Add(new Student(textBox_ime.Text, textBox_priimek.Text, "moški", new DateTime(1990, 2, 12)));
+            b.Add(new Student(textBox_ime.Text, textBox_priimek.Text, izbirnikSpola.Text, new DateTime(1990, 2, 12)));
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void textBox_ime_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
