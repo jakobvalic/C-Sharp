78c91ee7bd12b722a8f4f58dbc3313cf10d955c3
Jakob Valič <jakob.valic@gmail.com>
2018-01-19 08:19:37 +0100

    dodal 9. vajo

---
+++ vaja9-knjiznica/Glavni program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seznam;
+
+namespace Glavni_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
